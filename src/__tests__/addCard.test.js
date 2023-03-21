743a6e9a0d5388045aaefa6ed40d6ce3115d5ef6
mdg0410 <levoyersteven@gmail.com>
2023-03-21 01:56:58 -0500

    Create logic enviroment test

--- src/__tests__/addCard.test.js
+++ src/__tests__/addCard.test.js
@@ -1,5 +1,3 @@
-/* @jest-environment jsdom */
-
 import project from '../modules/Card-test.js';
 import addCard from '../__Mocks__/addCard-Mock.js';
 import localStorageMock from '../__Mocks__/localStorageMock.js';
@@ -7,19 +5,26 @@ import localStorageMock from '../__Mocks__/localStorageMock.js';
 describe('Add Cards Validator', () => {
   // Test Add new project
   test('Add new project should be one', () => {
-    expect(addCard(project)).toHaveLength(1);
+    expect(addCard(project[0])).toBe(1);
   });
 
   // Test if the new project is save in localstorage
   test('The value of the first card is expected to be one', () => {
-    expect(localStorageMock.getFromLocal('test')).toHaveLength(1);
+    expect(localStorageMock.getFromLocal('test')).toHaveProperty('index', 1);
   });
 
+  // Test Add new project
   test('Add new project should be two', () => {
-    expect(addCard(project)).toHaveLength(2);
+    expect(addCard(project[1])).toBe(2);
   });
 
+  // Test if the new project is save in localstorage
   test('The value of the second card is expected to be two', () => {
-    expect(localStorageMock.getFromLocal('test')[1].index).toEqual(2);
+    expect(localStorageMock.getFromLocal('test')).toHaveProperty('index', 2);
+  });
+
+  // Expect for error not title
+  test('Error not title', () => {
+    expect(() => addCard(project[2])).toThrowError();
   });
 });
